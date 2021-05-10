ee27539cb2d7c79b208fcf36cc876f85e12efc04
LuyandaDingindlela101 <3995219@myuwc.ac.za>
2021-05-10 10:17:35 +0200

    added files from previous repo

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// Initialize fullpage Fullpage
+new fullpage("#fullpage", {
+	anchors: ["first", "seccond", "third"],
+	navigation: true,
+	navigationPosition: "left",
+	navigationTooltips: ["first", "seccond", "third"],
+	center: true,
+});
