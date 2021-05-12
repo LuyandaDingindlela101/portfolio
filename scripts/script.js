6995bcef785fbb9e77f7f3edad355d9ad75fab84
LuyandaDingindlela101 <3995219@myuwc.ac.za>
2021-05-12 16:32:32 +0200

    responsiveness is done, now working on light and dark mode toggle

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
-// FUNCTION TO MAKE THE THEME CHANGE
-
 // Toggle Menu for mobile function
 function toggleMenu(){
 	document.getElementsByClassName('nav-list')[0].classList.toggle('active')
+}
+
+// FUNCTION TO MAKE THE THEME CHANGE
+function toggleTheme() {
+		
 }
