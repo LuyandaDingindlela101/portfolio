a5faab09a22be11bb8bc69ecfad41fdd739e77fb
LuyandaDingindlela101 <3995219@myuwc.ac.za>
2021-05-13 10:42:16 +0200

    dark mode complete, now working on animating the progress bars

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,22 @@
-// Toggle Menu for mobile function
+//	Toggle Menu for mobile function
 function toggleMenu(){
 	document.getElementsByClassName('nav-list')[0].classList.toggle('active')
 }
 
+//	IN THE document OBJECT, SELECT THE body ELEMENT AND ADD AN EVEN LISTENER FOR THE 
+// 	keyup EVENT, THEN RUN THE CALLBACK FUNCTION
+document.body.addEventListener('keyup', function(e) {
+	// IF THE e(EVENT) code IS EQUAL TO Space, THEN CHANGE THE THEME
+	if(e.code == "Space") {
+		// 	CALL THE toggleTheme() FUNCTION
+		toggleTheme();
+	}
+})
+
+
 // FUNCTION TO MAKE THE THEME CHANGE
 function toggleTheme() {
-		
+	//	IN THE document OBJECT, SELECT THE body ELEMENT AND ADD AN EVEN LISTENER FOR THE 
+	// 	keyup EVENT, THEN toggle THE dark-mode CLASS
+	document.body.classList.toggle('dark-mode');	
 }
