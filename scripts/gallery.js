4ba4dbbe16df66e18b1aa0566e97c285e0cd586f
LuyandaGitHub <49154286+LuyandaGitHub@users.noreply.github.com>
2021-06-04 23:51:17 +0200

    added filter buttons to gallery and now working on the filter functionality

---
+++ scripts/gallery.js
@@ -0,0 +1,4 @@
+// GET ALL THE FILTER BUTTONS
+let all_btn = document.querySelector(".all-btn");
+let python_btn = document.querySelector(".python-btn");
+let web_design_btn = document.querySelector(".web-design-btn");
