1801010384dc9bf27450240713fe02d0de046938
Luyanda <LuyandaDingindlela@gmail.com>
2021-10-06 21:18:45 +0200

    replaced two projects

--- scripts/script.js
+++ scripts/script.js
@@ -9,20 +9,13 @@ function toggleMenu() {
 	document.querySelector(".nav-container").classList.toggle("active");
 }
 
-//	IN THE document OBJECT, SELECT THE body ELEMENT AND ADD AN EVEN LISTENER FOR THE 
-// 	keyup EVENT, THEN RUN THE CALLBACK FUNCTION
-document.body.addEventListener('keyup', function(e) {
-	// IF THE e(EVENT) code IS EQUAL TO Space, THE tagName IS NOT EQUAL TO INPUT AND T THEN CHANGE THE THEME
-	if(e.code == "Space" &&  e.target.tagName.toUpperCase() !== "INPUT" && e.target.tagName.toUpperCase() !== "TEXTAREA") {
-		// 	CALL THE toggleTheme() FUNCTION
-		toggleTheme();
-	}
-})
 document.body.addEventListener('keydown', function(e) {
 	// IF THE e(EVENT) code IS EQUAL TO Space, THE tagName IS NOT EQUAL TO INPUT AND T THEN CHANGE THE THEME
 	if(e.code == "Space" &&  e.target.tagName.toUpperCase() !== "INPUT" && e.target.tagName.toUpperCase() !== "TEXTAREA") {
 		// 	CALL THE preventDefault() FUNCTION WHICH WILL STOP THAT ANOYING SCROLL WHEN YOU PRESS THE SPACE BAR
 		e.preventDefault();
+		toggleTheme();
+
 	}
 })
 
