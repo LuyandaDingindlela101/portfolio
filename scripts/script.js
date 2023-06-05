9dd29454d928e7b1aaa5e7d42d92727fbd269756
notLuyanda <luyandadingindlela@gmail.com>
2023-06-05 20:26:57 +0200

    Added new project and removed useless info

--- scripts/script.js
+++ scripts/script.js
@@ -34,34 +34,6 @@ function toggleTheme() {
 	}
 }
 
-
-// 	CREATE A VARIABLE FOR custom_cursor TO HOLD THE .custom-cursor
-const custom_cursor = document.querySelector(".custom-cursor");
-// FUNCTION TO MAKE THE custom_cursor MOVEWITH THE MOUSE
-document.addEventListener('mousemove', function(e) {
-	// 	DYNAMICALLY ADJUST THE TOP POSITION OF THE custom_cursor BASED ON THE ACTUAL MOUSE
-	custom_cursor.style.top = (e.pageY- 10) + "px";
-	// 	DYNAMICALLY ADJUST THE LEFT POSITION OF THE custom_cursor BASED ON THE ACTUAL MOUSE
-	custom_cursor.style.left = (e.pageX -10) + "px"; 
-})
-
-
-// 	querySelectorAll RETURNS A LIST[] OF ALL THE a TAGS
-const site_links = document.querySelectorAll("a")
-// 	HERE, WE LOOP THROUGH THE LIST 
-site_links.forEach(link => {
-	//	ADD A mouseover EVENT LISTENER TO EACH INDIVIDUAL link
-	link.addEventListener("mouseover", function() {
-		// 	WHEN THE link IS HOVERED ON, ADD THE expand CLASS
-		custom_cursor.classList.add("expand");
-	})
-	//	ADD A mouseover EVENT LISTENER TO EACH INDIVIDUAL link
-	link.addEventListener("mouseleave", function() {
-		// 	WHEN THE MOUSE LEAVES THE Link, REMOVE THE expand CLASS
-		custom_cursor.classList.remove("expand");
-	})
-});
-
 const nav_links = document.querySelectorAll(".nav-link");
 
 nav_links.forEach(link => {
@@ -75,6 +47,7 @@ nav_links.forEach(link => {
 
 // 	CREATE A VARIABLE FOR preloader TO HOLD THE .preloader
 const preloader = document.querySelector('.preloader');
+
 // 	FUNCTION FOR THE preloader EFFECT THAT HAPPENS EVERY 100 SECONDS
 const fadeEffect = setInterval(() => {
 	// 	IF THE preloader DOESN'T HAVE THE OPACITY THEN IT WILL BE EQUAL TO AN EMPTY STRING
